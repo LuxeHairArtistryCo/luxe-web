b70be09a18a4b314beb656d7f422929f9d7aa77a
Nathan Marttunen <nemarttunen@gmail.com>
2026-06-28 17:04:46 -0400

    Added site url as env var

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,13 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+	const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 	const isPreview = process.env.VERCEL_URL?.includes('preview');
 
 	return {
 		rules: isPreview
 			? { userAgent: '*', disallow: '/' }
 			: { userAgent: '*', allow: '/' },
-		sitemap: 'https://luxehairartistry.ca/sitemap.xml',
+		sitemap: `${baseUrl}/sitemap.xml`,
 	}
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ import { ARTIST_QUERY } from '@/lib/queries'
 import { Artist } from '@/lib/types'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const baseUrl = 'https://luxehairartistry.ca';
+	const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
 	const artists: Artist[] = await sanityClient.fetch(ARTIST_QUERY);
 
